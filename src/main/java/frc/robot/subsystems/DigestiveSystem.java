bf1565650489369cade507a19066b06304278382
NobleQuester <twhelgeson@gmail.com>
2022-01-24 10:11:11 -0500

    Replaced SmartDashboard statements with Oblog

--- src/main/java/frc/robot/subsystems/DigestiveSystem.java
+++ src/main/java/frc/robot/subsystems/DigestiveSystem.java
@@ -15,34 +15,37 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CAN;
 import frc.robot.Constants.Shooter;
+import io.github.oblarg.oblog.Loggable;
+import io.github.oblarg.oblog.annotations.Config;
+import io.github.oblarg.oblog.annotations.Log;
 
-public class DigestiveSystem extends SubsystemBase {
+public class DigestiveSystem extends SubsystemBase implements Loggable {
 
   private final CANSparkMax intake = new CANSparkMax(CAN.intake, MotorType.kBrushless);
   private final CANSparkMax transfer = new CANSparkMax(CAN.transfer, MotorType.kBrushless);
   private final CANSparkMax flywheel1 = new CANSparkMax(CAN.flywheelL, MotorType.kBrushless);
   private final CANSparkMax flywheel2 = new CANSparkMax(CAN.flywheelR, MotorType.kBrushless);
 
-  private final RelativeEncoder intake_ENC;
-  private final RelativeEncoder transfer_ENC;
-  private final RelativeEncoder flywheel_ENC;
+  private final RelativeEncoder intake_ENC = intake.getEncoder();
+  private final RelativeEncoder transfer_ENC = transfer.getEncoder();
+  private final RelativeEncoder flywheel_ENC = flywheel1.getEncoder();
 
   private boolean beam1Broken = false;
   private boolean beam2Broken = false;
 
-  // private double flywheelRPM = 0;
-
-  // private double lastPosition = 0;
-
-  // private Timer loopTime = new Timer();
-
   PIDController flywheelPID = new PIDController(0.1, 0, 0);
   BangBangController flywheelBangBang = new BangBangController();
+
+  @Config private double flywheelTargetSpeedRPM;
+  private double flywheelTargetSpeedRPS;
+  private double flywheelSpeedRPS;
+  private double flywheelFFEffort;
+  private double flywheelBBEffort;
+  @Log private double flywheelTotalEffort;
 
   SimpleMotorFeedforward feedForward =
       new SimpleMotorFeedforward(Shooter.ksVolts, Shooter.kvVoltSecondsPerRotation);
 
-  /** Creates a new Intake. */
   public DigestiveSystem() {
 
     intake.restoreFactoryDefaults();
@@ -55,48 +58,28 @@ public class DigestiveSystem extends SubsystemBase {
     flywheel1.setIdleMode(IdleMode.kCoast);
     flywheel2.setIdleMode(IdleMode.kCoast);
 
-    intake_ENC = intake.getEncoder();
-    transfer_ENC = transfer.getEncoder();
-    flywheel_ENC = flywheel1.getEncoder();
-
-    SmartDashboard.putNumber("flywheel/SpeedSetpoint", 0);
-    SmartDashboard.putNumber("flywheel/VoltageInput", 0);
-    SmartDashboard.putNumber("flywheel/kP", flywheelPID.getP());
-    SmartDashboard.putNumber("transfer/speed", 0);
-
     flywheel1.setInverted(true);
     flywheel2.follow(flywheel1, true);
 
-    // loopTime.start();
   }
 
-  public void setSpeed(double speed) {
-    flywheel1.set(speed);
+  @Config
+  public void setIntakeSpeedProp(double prop) {
+    intake.set(prop);
   }
 
-  public void stop() {
-    flywheel1.setVoltage(0);
+  @Config
+  public void setTransferSpeedProp(double prop) {
+    transfer.set(prop);
   }
 
-  // intake methods
-  public void takeIn(double pwr) {
-    intake.set(pwr);
+  @Config
+  public void setFlywheelTargetSpeedRPM(double RPM) {
+    flywheelTargetSpeedRPM = RPM;
   }
 
   @Override
   public void periodic() {
-    double targetSpeed = SmartDashboard.getNumber("flywheel/SpeedSetpoint", 0.0);
-
-    // setSpeed(SmartDashboard.getNumber("flywheel/SpeedSetpoint", 0.0));
-    SmartDashboard.putNumber("flywheel/Vel", flywheel_ENC.getVelocity());
-    SmartDashboard.putNumber("flywheel/Pos", flywheel_ENC.getPosition());
-
-    // flywheelRPM = ((flywheel_ENC.getPosition() - lastPosition) / loopTime.get())
-    // * 60;
-    // loopTime.reset();
-    // lastPosition = flywheel_ENC.getPosition();
-
-    SmartDashboard.putNumber("flywheel/RPM", flywheel_ENC.getVelocity());
 
     /*
      * // This method will be called once per scheduler run
@@ -113,27 +96,14 @@ public class DigestiveSystem extends SubsystemBase {
      * flywheel.set(0);
      * }
      */
-    // getting values for ff from smart dashboard
-    // flywheel.set(bangBang.calculate(flywheel_ENC.getVelocity(), targetSpeed) +
-    // 0.9 * feedForward.calculate(targetSpeed));
 
-    // flywheel1.setVoltage(SmartDashboard.getNumber("flywheel/VoltageInput", 0));
-    transfer.set(SmartDashboard.getNumber("transfer/speed", 0));
+    flywheelTargetSpeedRPS = flywheelTargetSpeedRPM / 60;
+    flywheelSpeedRPS = flywheel_ENC.getVelocity() / 60;
 
-    double targetSpeedRPS = targetSpeed / 60;
-    double currentSpeedRPS = flywheel_ENC.getVelocity() / 60;
-    SmartDashboard.putNumber("flywheel/velRPS", currentSpeedRPS);
-    SmartDashboard.putNumber("flywheel/targetSpeedRPS", targetSpeedRPS);
+    flywheelFFEffort = feedForward.calculate(flywheelTargetSpeedRPS);
+    flywheelBBEffort = flywheelBangBang.calculate(flywheelSpeedRPS, flywheelTargetSpeedRPS);
+    flywheelTotalEffort = flywheelFFEffort + flywheelBBEffort * 0.25;
+    flywheel1.setVoltage(flywheelTotalEffort);
 
-    flywheelPID.setP(SmartDashboard.getNumber("flywheel/kP", flywheelPID.getP()));
-
-    double ffEffort = feedForward.calculate(targetSpeedRPS);
-    double bbEffort = flywheelBangBang.calculate(currentSpeedRPS, targetSpeedRPS);
-    double totalEffort = ffEffort + bbEffort * 0.25;
-    flywheel1.setVoltage(totalEffort);
-
-    SmartDashboard.putNumber("flywheel/ffEffort", ffEffort);
-    SmartDashboard.putNumber("flywheel/bbEffort", bbEffort);
-    SmartDashboard.putNumber("flywheel/totalControlEffort", totalEffort);
   }
 }
